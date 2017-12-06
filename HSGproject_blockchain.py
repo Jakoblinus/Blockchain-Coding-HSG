e3afdefdd12bc49d0d248a672d28eba8d569e7ef
phillipkenzel <33482768+phillipkenzel@users.noreply.github.com>
2017-12-06 23:05:43 +0100

    Update HSGproject_blockchain.py

--- HSGproject_blockchain.py
+++ HSGproject_blockchain.py
@@ -1,1 +1,16 @@
-print("helloworld")
+import hashlib as hasher
+import datetime as date
+import random
+import json
+
+a = str(input("Your name please: "))
+usernames = ["Jordi", "Phillip"]
+userdata = ["ID: 1, money available: 200", "id: 2, money: 100"]
+if a == "Jordi" in usernames:
+  print("Hello", a)
+  print(userdata[0])
+elif a == "Phillip" in usernames:
+  print("Hello", a)
+  print(userdata[1])
+else:
+    print("You are not a registered user.")
